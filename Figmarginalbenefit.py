7b0bcbc079b55a3b2ebca6942f465e5ebb52fd0e
joel miller <joelmiller@users.noreply.github.com>
2022-11-06 17:29:12 +1100

    Update Figmarginalbenefit.py
    Correct bug in calculation of total benefit and improve appearance of figures.

--- Figmarginalbenefit.py
+++ Figmarginalbenefit.py
@@ -35,7 +35,7 @@ i = 0
 
 theta = 0.16
 
-pep = np.ones(lenRc).reshape(1,lenRc)
+pep = np.ones(lenRc).reshape(1,lenRc) #probability of epidemic for Poisson
 while np.min(1 - np.exp(-Rc*(pep - errbnd)) - pep + errbnd) < 0 and np.max(pep - errbnd) > 0 and i < maxit:
     pep = ((Rc*pep + 1)*np.exp(-Rc*pep) - 1)/(Rc*np.exp(-Rc*pep) - 1)
     i += 1
@@ -47,7 +47,7 @@ i = 0
 if i == maxit:
     raise Exception('Calculation took too long. (Poisson)')
 
-penb = np.ones(lenRc).reshape(1,lenRc)
+penb = np.ones(lenRc).reshape(1,lenRc) #Probability of Epidemic for Negative Binomial
 while np.min(1 - (q/(q + p*(penb - errbnd)))**r - penb + errbnd) < 0 and np.max(penb - errbnd) > 0 and i < maxit:
     penb = penb - (1 - (q/(q + p*penb))**r - penb)/(r*q**r*p/(q + p*penb)**(r + 1) - 1)
     i += 1
@@ -78,8 +78,8 @@ for i1 in range(lenRc):
     ax0.semilogy(E, Benefit_p[:,i1])
     ax1.semilogy(E, Benefit_nb[:,i1], label = r'$\mathcal{R}_c=' + str(Rc[i1]) +'$')
 
-ax.set_xlabel('$L-1$')
-ax.set_ylabel('Reduction in epidemic probability from one person isolating')
+ax.set_xlabel('Number of other introductions', FontSize=12)
+ax.set_ylabel('Reduction in epidemic probability from one infected person isolating', FontSize=12)
 ax.yaxis.labelpad = 15
 ax0.set_xlim([0, maxE])
 ax1.set_xlim([0, maxE])
@@ -101,8 +101,9 @@ Next_margben_p=pep*(1 - pep)**(Tot_infected - Isolating[:Tot_infected] - 1)
 Av_reduction_nb=1/Isolating[1:]*(1 - (1 - penb)**Isolating[1:])*(1 - penb)**(Tot_infected - Isolating[1:])
 Next_margben_nb=penb*(1 - penb)**(Tot_infected - Isolating[:Tot_infected] - 1)
 
-Tot_prob_p = 1 - (1-pep)**(Tot_infected-Isolating)
-Tot_prob_nb = 1 - (1-penb)**(Tot_infected-Isolating)
+Tot_benefit_p = (1-pep)**(Tot_infected-Isolating) - (1-pep)**(Tot_infected)
+Tot_benefit_nb = (1-penb)**(Tot_infected-Isolating)- (1-penb)**(Tot_infected)
+
 
 fig = plt.figure(figsize = (5.8, 7.5))
 ax = fig.add_subplot(111)
@@ -117,25 +118,31 @@ ax.tick_params(labelcolor='w', top=False, bottom=False, left=False, right=False)
 
 plt.gcf().subplots_adjust(top = 0.92, bottom = 0.09, left = 0.14, right = 0.77)
 for i1 in range(lenRc):
-    ax0.semilogy(Isolating[1:], Av_reduction_p[:,i1], 'C' + str(i1) + '--')
-    ax0.semilogy(Isolating[:Tot_infected], Next_margben_p[:,i1], 'C' + str(i1)+ '-.')
-    ax0.semilogy(Isolating, 1-Tot_prob_p[:,i1], 'C' + str(i1))
+    if i1 == 0:
+        ax0.semilogy(Isolating, Tot_benefit_p[:,i1], 'C' + str(i1),label='Total \nbenefit')
+        ax0.semilogy(Isolating[1:], Av_reduction_p[:,i1], 'C' + str(i1) + '--', label = 'Average \nbenefit')
+        ax0.semilogy(Isolating[:Tot_infected], Next_margben_p[:,i1], 'C' + str(i1)+ '-.', label = 'Marginal \nbenefit')
+    else:
+        ax0.semilogy(Isolating[1:], Av_reduction_p[:,i1], 'C' + str(i1) + '--')
+        ax0.semilogy(Isolating[:Tot_infected], Next_margben_p[:,i1], 'C' + str(i1)+ '-.')
+        ax0.semilogy(Isolating, Tot_benefit_p[:,i1], 'C' + str(i1))
     ax1.semilogy(Isolating[1:], Av_reduction_nb[:,i1], 'C' + str(i1) + '--')
     ax1.semilogy(Isolating[:Tot_infected], Next_margben_nb[:,i1], 'C' + str(i1) + '-.')
-    ax1.semilogy(Isolating, 1-Tot_prob_nb[:,i1], 'C' + str(i1), label = r'$\mathcal{R}_c=' + str(Rc[i1]) +'$')
+    ax1.semilogy(Isolating, Tot_benefit_nb[:,i1], 'C' + str(i1), label = r'$\mathcal{R}_c=' + str(Rc[i1]) +'$')
 
-ax.set_xlabel('$M$')
-ax.set_ylabel('Benefit')
+ax.set_xlabel('Number isolating (of 50 introductions)', FontSize=12)
+ax.set_ylabel('Benefit', FontSize=12)
 ax.yaxis.labelpad = 15
 
 ax0.set_ylim([1e-3,1])
 ax1.set_ylim([1e-3,1])
 
-ax0.set_xlim([Tot_infected-10, Tot_infected])
+#ax0.set_xlim([Tot_infected-10, Tot_infected])
+ax0.set_xlim([35, Tot_infected])
 ax1.set_xlim([1, Tot_infected])
 ax0.set_title('Poisson offspring distribution')
 ax1.set_title('Negative binomial offspring distribution')
-ax1.legend(bbox_to_anchor=(1.01, 1.1), loc='center left')
-
+ax1.legend(bbox_to_anchor=(1.01, 1.0), loc='upper left')
+ax0.legend(bbox_to_anchor=(1.01,0.4), loc='upper left')
 
 plt.savefig('ProbMargAvbenefitlog' + str(Tot_infected) + '.png')
